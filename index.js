f62c2b5a3a304aa1e9faa28c4a43f7649be65ff6
+ <escobedo.jose48@outlook.com>
2022-07-14 04:32:34 -0700

    instock to product schema

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productsRoute = require("./routes/product");
 const cartsRoute = require("./routes/cart");
 const ordersRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 dotenv.config();
 
 mongoose
@@ -16,12 +18,14 @@ mongoose
     console.log(err);
   });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", usersRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productsRoute);
 app.use("/api/carts", cartsRoute);
 app.use("/api/orders", ordersRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("backend is running!");

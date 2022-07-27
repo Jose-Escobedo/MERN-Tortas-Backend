8f59964dfd6b58c230547f7b4c0020bc740b6a3e
+ <escobedo.jose48@outlook.com>
2022-07-27 15:43:50 -0700

    order and user changes

--- routes/order.js
+++ routes/order.js
@@ -67,7 +67,7 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
 router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
   const date = new Date();
   const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
-  const previousMonth = new Date(new date().setMonth(lastMonth.getMonth() - 1));
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
   try {
     const data = await User.aggregate([
       { $match: { createdAt: { $gte: previousMonth } } },

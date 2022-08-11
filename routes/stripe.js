6f969b5600019bf626fa999e45973d769e2fbc6e
+ <escobedo.jose48@outlook.com>
2022-08-10 23:03:19 -0700

    stripe cart total

--- routes/stripe.js
+++ routes/stripe.js
@@ -6,23 +6,30 @@ const router = require("express").Router();
 // Sign in to see your own test API key embedded in code samples.
 const stripe = require("stripe")(KEY);
 
-const calculateOrderAmount = (items) => {
-  // Replace this constant with a calculation of the order's amount
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-  return 1400;
-};
+// const calculateOrderAmount = (items) => {
+//   // Replace this constant with a calculation of the order's amount
+//   // Calculate the order total on the server to prevent
+//   // people from directly manipulating the amount on the client
+//   console.log(items);
+//   return items;
+// };
 
 router.post("/payment", async (req, res) => {
   const { line_items } = req.body;
+  console.log(req.body.cart);
+  const toCent = (item) => {
+    const str = item.toString();
+    const int = str.split(".");
 
+    return Number(item.replace(".", "").padEnd(int.length === 1 ? 3 : 4, "0"));
+  };
   // Create a PaymentIntent with the order amount and currency
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         price_data: {
           currency: "usd",
-          unit_amount: 500,
+          unit_amount: toCent(req.body.total),
           product_data: {
             name: "Order Total",
           },

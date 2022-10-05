de01f089007718ba87e44dc7dd8587b5188c22a9
+ <escobedo.jose48@outlook.com>
2022-10-04 19:57:32 -0700

    order router post fix bug

--- routes/order.js
+++ routes/order.js
@@ -11,7 +11,7 @@ router.post("/", verifyToken, async (req, res) => {
   const newOrder = new Order(req.body);
   try {
     const savedOrder = await newOrder.save();
-    res.status(200).json(savedProduct);
+    res.status(200).json(savedOrder);
   } catch (err) {
     res.status(500).json(err);
   }

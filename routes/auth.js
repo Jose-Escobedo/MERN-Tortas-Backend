9435c703043e924e13bd8c5a76baeaa1d0cf40aa
+ <escobedo.jose48@outlook.com>
2022-11-03 16:09:21 -0700

    order post changed to check prices

--- routes/auth.js
+++ routes/auth.js
@@ -26,41 +26,48 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username });
-    !user && res.status(401).json("Wrong credentials!");
+    console.log("user:-", req.body.username);
 
-    const hashedPassword = CryptoJS.AES.decrypt(
-      user.password,
-      process.env.SECRET_PASS
-    );
-    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    User.findOne({ username: req.body.username }).exec((err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          error: "User with that email does not exist. Please signup",
+        });
+      }
+      const hashedPassword = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.SECRET_PASS
+      );
+      const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-    OriginalPassword !== req.body.password &&
-      res.status(401).json("Wrong credentials!");
+      if (OriginalPassword !== req.body.password) {
+        return res.status(401).json("Wrong credentials!");
+      }
 
-    const accessToken = jwt.sign(
-      {
-        id: user._id,
-        isAdmin: user.isAdmin,
-      },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "1d" }
-    );
-    const refreshToken = jwt.sign(
-      {
-        id: user._id,
-        isAdmin: user.isAdmin,
-      },
-      process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: "7d" }
-    );
+      const accessToken = jwt.sign(
+        {
+          id: user._id,
+          isAdmin: user.isAdmin,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+      const refreshToken = jwt.sign(
+        {
+          id: user._id,
+          isAdmin: user.isAdmin,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "7d" }
+      );
 
-    const { password, ...others } = user._doc;
-    res.cookie("jwt", refreshToken, {
-      httpOnly: true,
-      maxAge: 24 * 60 * 60 * 1000,
+      const { password, ...others } = user._doc;
+      res.cookie("jwt", refreshToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+      res.status(200).json({ ...others, accessToken });
     });
-    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }

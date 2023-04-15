efef819f5d70d8d581a751748ab4f7acfd590d50
+ <escobedo.jose48@outlook.com>
2023-04-14 20:04:18 -0700

    deletign dependcencios

--- index.js
+++ index.js
@@ -12,16 +12,6 @@ const adminRoute = require("./routes/admin");
 const doordashRoute = require("./routes/doordashGet");
 const cors = require("cors");
 
-const http = require("http");
-const { Server } = require("socket.io");
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3006",
-  },
-});
-
 dotenv.config();
 const corsOptions = {
   origin: ["http://localhost:3006", "http://localhost:3007"],
@@ -43,10 +33,6 @@ app.use(
     },
   })
 );
-
-io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
-});
 
 app.use("/api/users", usersRoute);
 app.use("/api/auth", authRoute);

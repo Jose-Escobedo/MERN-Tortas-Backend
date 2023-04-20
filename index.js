66b5e3e2aaa28297e6ba9fdf6e1043ed6412fe6a
+ <escobedo.jose48@outlook.com>
2023-04-19 23:24:25 -0700

    editing enpoints

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 
 dotenv.config();
 const corsOptions = {
-  origin: ["https://tortasfrontend.herokuapp.com"],
+  origin: ["https://www.tortasmexico-studiocity.com"],
   optionSuccessStatus: 200,
 };
 

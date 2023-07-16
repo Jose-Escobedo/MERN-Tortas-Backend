0cd539670ffea96cd08274b0a97ce6849f55c49d
+ <escobedo.jose48@outlook.com>
2023-07-15 19:00:45 -0700

    changing to https

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const adminRoute = require("./routes/admin");
 const doordashRoute = require("./routes/doordashGet");
 const cors = require("cors");
 
-const http = require("http");
+const https = require("https");
 const { Server } = require("socket.io");
-const server = http.createServer(app);
+const server = https.createServer(app);
 
 const io = new Server(server, {
   cors: {

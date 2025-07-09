c361482e97d7a4a33435bdef16fc026def1bdc37
jose-escobedo <escobedo.jose48@outlook.com>
2025-07-09 16:23:07 -0700

    commenting out further.

--- routes/admin.js
+++ routes/admin.js
@@ -1,51 +1,51 @@
-const router = require("express").Router();
-const Order = require("../models/Order");
-const jwt = require("jsonwebtoken");
-const axios = require("axios");
+// const router = require("express").Router();
+// const Order = require("../models/Order");
+// const jwt = require("jsonwebtoken");
+// const axios = require("axios");
 
-const accessKey = {
-  developer_id: process.env.DEVELOPER_DOORDASH,
-  key_id: process.env.ACCESS_DOORDASH,
-  signing_secret: process.env.SECRET_DOORDASH,
-};
+// const accessKey = {
+//   developer_id: process.env.DEVELOPER_DOORDASH,
+//   key_id: process.env.ACCESS_DOORDASH,
+//   signing_secret: process.env.SECRET_DOORDASH,
+// };
 
-const data = {
-  aud: "doordash",
-  iss: accessKey.developer_id,
-  kid: accessKey.key_id,
-  exp: Math.floor(Date.now() / 1000 + 60),
-  iat: Math.floor(Date.now() / 1000),
-};
+// const data = {
+//   aud: "doordash",
+//   iss: accessKey.developer_id,
+//   kid: accessKey.key_id,
+//   exp: Math.floor(Date.now() / 1000 + 60),
+//   iat: Math.floor(Date.now() / 1000),
+// };
 
-const headers = { algorithm: "HS256", header: { "dd-ver": "DD-JWT-V1" } };
+// const headers = { algorithm: "HS256", header: { "dd-ver": "DD-JWT-V1" } };
 
-const token = jwt.sign(
-  data,
-  Buffer.from(accessKey.signing_secret, "base64"),
-  headers
-);
-const {
-  verifyToken,
-  verifyTokenAndAuthorization,
-  verifyTokenAndAdmin,
-} = require("./verifyToken");
+// const token = jwt.sign(
+//   data,
+//   Buffer.from(accessKey.signing_secret, "base64"),
+//   headers
+// );
+// const {
+//   verifyToken,
+//   verifyTokenAndAuthorization,
+//   verifyTokenAndAdmin,
+// } = require("./verifyToken");
 
-//
-router.get("/Orders", verifyTokenAndAdmin, async (req, res) => {
-  try {
-    const orders = await Order.find();
-    return res.status(200).json(orders);
-  } catch (err) {
-    return res.status(500).json(err);
-  }
-});
+// //
+// router.get("/Orders", verifyTokenAndAdmin, async (req, res) => {
+//   try {
+//     const orders = await Order.find();
+//     return res.status(200).json(orders);
+//   } catch (err) {
+//     return res.status(500).json(err);
+//   }
+// });
 
-router.get("/doordashPatch", verifyTokenAndAdmin, async (req, res) => {
-  try {
-    return res.status(200).json(token);
-  } catch (err) {
-    console.log(err);
-  }
-});
+// router.get("/doordashPatch", verifyTokenAndAdmin, async (req, res) => {
+//   try {
+//     return res.status(200).json(token);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
--- routes/auth.js
+++ routes/auth.js
@@ -1,89 +1,89 @@
-const router = require("express").Router();
-const User = require("../models/User");
-const CryptoJS = require("crypto-js");
-const jwt = require("jsonwebtoken");
+// const router = require("express").Router();
+// const User = require("../models/User");
+// const CryptoJS = require("crypto-js");
+// const jwt = require("jsonwebtoken");
 
-//REGISTER
-router.post("/register", async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    name: req.body.name,
-    lastname: req.body.lastname,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.SECRET_PASS
-    ).toString(),
-  });
+// //REGISTER
+// router.post("/register", async (req, res) => {
+//   const newUser = new User({
+//     username: req.body.username,
+//     email: req.body.email,
+//     name: req.body.name,
+//     lastname: req.body.lastname,
+//     password: CryptoJS.AES.encrypt(
+//       req.body.password,
+//       process.env.SECRET_PASS
+//     ).toString(),
+//   });
 
-  try {
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//   try {
+//     const savedUser = await newUser.save();
+//     res.status(201).json(savedUser);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-//LOGIN
+// //LOGIN
 
-router.post("/login", async (req, res) => {
-  try {
-    console.log("user:-", req.body.username);
+// router.post("/login", async (req, res) => {
+//   try {
+//     console.log("user:-", req.body.username);
 
-    User.findOne({ username: req.body.username }).exec((err, user) => {
-      if (err || !user) {
-        return res.status(400).json({
-          error: "User with that email does not exist. Please signup",
-        });
-      }
-      const hashedPassword = CryptoJS.AES.decrypt(
-        user.password,
-        process.env.SECRET_PASS
-      );
-      const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+//     User.findOne({ username: req.body.username }).exec((err, user) => {
+//       if (err || !user) {
+//         return res.status(400).json({
+//           error: "User with that email does not exist. Please signup",
+//         });
+//       }
+//       const hashedPassword = CryptoJS.AES.decrypt(
+//         user.password,
+//         process.env.SECRET_PASS
+//       );
+//       const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-      if (OriginalPassword !== req.body.password) {
-        return res.status(401).json("Wrong credentials!");
-      }
+//       if (OriginalPassword !== req.body.password) {
+//         return res.status(401).json("Wrong credentials!");
+//       }
 
-      const accessToken = jwt.sign(
-        {
-          id: user._id,
-          isAdmin: user.isAdmin,
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "7d" }
-      );
-      const refreshToken = jwt.sign(
-        {
-          id: user._id,
-          isAdmin: user.isAdmin,
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: "7d" }
-      );
+//       const accessToken = jwt.sign(
+//         {
+//           id: user._id,
+//           isAdmin: user.isAdmin,
+//         },
+//         process.env.ACCESS_TOKEN_SECRET,
+//         { expiresIn: "7d" }
+//       );
+//       const refreshToken = jwt.sign(
+//         {
+//           id: user._id,
+//           isAdmin: user.isAdmin,
+//         },
+//         process.env.REFRESH_TOKEN_SECRET,
+//         { expiresIn: "7d" }
+//       );
 
-      const { password, ...others } = user._doc;
-      res.cookie("jwt", refreshToken, {
-        httpOnly: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-      });
-      res.status(200).json({ ...others, accessToken });
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//       const { password, ...others } = user._doc;
+//       res.cookie("jwt", refreshToken, {
+//         httpOnly: true,
+//         maxAge: 7 * 24 * 60 * 60 * 1000,
+//       });
+//       res.status(200).json({ ...others, accessToken });
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-//LOGOUT
+// //LOGOUT
 
-router.get("/logout", async (req, res) => {
-  try {
-    res.clearCookie("jwt");
-    console.log("logout");
-  } catch (err) {
-    console.log(err);
-  }
-});
+// router.get("/logout", async (req, res) => {
+//   try {
+//     res.clearCookie("jwt");
+//     console.log("logout");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
